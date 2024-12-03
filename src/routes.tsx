03bd9ddb8b957e2e2516e5e36e05982d9f64d56e
aswds <sasha.iilllnitskiy@gmail.com>
2024-12-04 00:22:06 +0200

    routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,29 +4,32 @@ import MealPage from "./pages/MealPage";
 import { fetchRecipeById } from "./services/fetchRecipes";
 import FavoritesPage from "./pages/FavoritePage";
 
-let router = createBrowserRouter([
-  {
-    path: "/test-project-viso",
-    element: <MainPage />,
-  },
-  {
-    path: "recipe/:id",
-    element: <MealPage />,
-    loader: async ({ params }) => {
-      const recipeId = params.id;
-      try {
-        const recipe = await fetchRecipeById(recipeId);
-        console.log(recipe);
-        return recipe;
-      } catch (error) {
-        throw new Response("Not Found", { status: 404 });
-      }
+let router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainPage />,
     },
-  },
-  {
-    path: "/favorites",
-    element: <FavoritesPage />,
-  },
-]);
+    {
+      path: "recipe/:id",
+      element: <MealPage />,
+      loader: async ({ params }) => {
+        const recipeId = params.id;
+        try {
+          const recipe = await fetchRecipeById(recipeId);
+          console.log(recipe);
+          return recipe;
+        } catch (error) {
+          throw new Response("Not Found", { status: 404 });
+        }
+      },
+    },
+    {
+      path: "/favorites",
+      element: <FavoritesPage />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/test-project-viso" }
+);
 
 export { router };
